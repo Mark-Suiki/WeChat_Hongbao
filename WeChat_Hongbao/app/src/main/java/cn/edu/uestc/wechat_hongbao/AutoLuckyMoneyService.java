a970859b0b8068fec500b6fff9ba5fd41e02ab60
Mark-Suiki <unconscious_koishi@126.com>
2017-04-18 22:02:46 +0800

    唯一个问题。已领取红包已标记，但是因为在同一窗口中已领取，不会再离开聊天窗口。可能需要将这类节点主动从list中删除

--- WeChat_Hongbao/app/src/main/java/cn/edu/uestc/wechat_hongbao/AutoLuckyMoneyService.java
+++ WeChat_Hongbao/app/src/main/java/cn/edu/uestc/wechat_hongbao/AutoLuckyMoneyService.java
@@ -1,15 +1,26 @@
 package cn.edu.uestc.wechat_hongbao;
 
 import android.accessibilityservice.AccessibilityService;
+import android.app.KeyguardManager;
 import android.app.Notification;
 import android.app.PendingIntent;
-import android.support.annotation.NonNull;
+import android.content.Context;
+import android.os.Handler;
+import android.os.Message;
+import android.os.PowerManager;
 import android.util.Log;
 import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityNodeInfo;
 import android.widget.Toast;
 
+import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by admin on 2017/4/16.
@@ -17,6 +28,24 @@ import java.util.List;
 
 public class AutoLuckyMoneyService extends AccessibilityService {
     private final String TAG = getClass().getSimpleName();
+    public static int serviceMode;
+    private boolean flagOnGet = true;
+
+    private List<AccessibilityNodeInfo> nodeList;
+    private Collection<String> nodeHashName;
+    private MyHandler handler;
+
+    @Override
+    protected void onServiceConnected() {
+        super.onServiceConnected();
+
+        // 0 是普通模式， 1 是全取模式
+        serviceMode = 0;
+        nodeList = new ArrayList<>();
+        nodeHashName = new HashSet<>();
+        handler = new MyHandler(this);
+        Log.i(TAG, "Service connected");
+    }
 
     @Override
     public void onAccessibilityEvent(AccessibilityEvent accessibilityEvent) {
@@ -24,28 +53,42 @@ public class AutoLuckyMoneyService extends AccessibilityService {
 
         switch (accessibilityEvent.getEventType()) {
             case AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED:
+
+                if (!flagOnGet) break;
                 List<CharSequence> texts = accessibilityEvent.getText();
                 if (!texts.isEmpty()) {
                     for (CharSequence text : texts) {
                         if (text.toString().contains("[微信红包]")) {
+                            wakeUnlock(this);
                             openAppByNotification(accessibilityEvent);
                             break;
                         }
                     }
                 }
-                break;
+
             default:
                 String className = accessibilityEvent.getClassName().toString();
 
+                // TODO: 未来版本更新后报名可能会改变
                 // 在红包详情页之中，退出
-                if (className.equals("com.tencent.mm.plugin.luckymoney.ui.LuckyMoneyDetailUI"))
-                    goBackPage();
+                switch (className) {
+                    case "com.tencent.mm.plugin.luckymoney.ui.LuckyMoneyDetailUI":
+                        goBackPage();
+                        break;
 
-                // 在红包未拆待拆的界面
-                else if (className.equals("com.tencent.mm.plugin.luckymoney.ui.En_fba4b94f"))
-                    openPacket(accessibilityEvent);
+                    // 在红包已开待拆的界面
+                    case "com.tencent.mm.plugin.luckymoney.ui.LuckyMoneyReceiveUI":
+                    case "com.tencent.mm.plugin.luckymoney.ui.En_fba4b94f":
+                        openPacket();
+                        break;
 
-                // 在红包完全没有拆开的情况
+                    // 在红包完全没有拆开的情况
+                    case "com.tencent.mm.ui.LauncherUI":
+//                    default:
+                        AccessibilityNodeInfo rootNode = getRootInActiveWindow();
+                        getAllPackets(rootNode);
+                        break;
+                }
                 break;
         }
     }
@@ -55,22 +98,53 @@ public class AutoLuckyMoneyService extends AccessibilityService {
 
     }
 
-    private void getAllPackets() {
+    // -------------------------------------------------------------------------- //
 
+    private void wakeUnlock(Context context) {
+        KeyguardManager km = (KeyguardManager) context.getSystemService(Context.KEYGUARD_SERVICE);
+        KeyguardManager.KeyguardLock kl = km.newKeyguardLock("unLock");
+        //解锁
+        kl.disableKeyguard();
+        //获取电源管理器对象
+        PowerManager pm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+        //获取PowerManager.WakeLock对象,后面的参数|表示同时传入两个值,最后的是LogCat里用的Tag
+        PowerManager.WakeLock wl = pm.newWakeLock(PowerManager.ACQUIRE_CAUSES_WAKEUP | PowerManager.SCREEN_DIM_WAKE_LOCK, "bright");
+        //点亮屏幕
+        wl.acquire();
+        //释放
+        wl.release();
     }
 
-    private void openPacket(AccessibilityEvent _Event) {
+    private void goBackPage() {
+        AccessibilityNodeInfo nodeInfo = getRootInActiveWindow();
+        if (nodeInfo != null) {
+            performGlobalAction(GLOBAL_ACTION_BACK);
+            nodeInfo.recycle();
+        }
+    }
+
+    private void openPacket() {
         AccessibilityNodeInfo rootNode = getRootInActiveWindow();
         if (rootNode != null) {
-            List<AccessibilityNodeInfo> nodes = rootNode.findAccessibilityNodeInfosByText("领取红包");
-            if (!nodes.isEmpty()) {
-                for (AccessibilityNodeInfo infoNode : nodes) {
-                    AccessibilityNodeInfo clickableNode = findClickable(infoNode);
-                    clickableNode.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+            // TODO: 找到buttonNode对应的Button控件
+            // TODO: 在将来更新版本后必须修改id
+            List<AccessibilityNodeInfo> buttonNodes = rootNode.findAccessibilityNodeInfosByViewId("com.tencent.mm:id/bi3");
+            if (!buttonNodes.isEmpty()) {
+                AccessibilityNodeInfo buttonNode = buttonNodes.get(0);
+                if (isButton(buttonNode)) {
+                    buttonNode.performAction(AccessibilityNodeInfo.ACTION_CLICK);
                 }
+            } else {
+                goBackPage();
             }
         }
     }
+
+    private boolean isButton(AccessibilityNodeInfo node) {
+        return node.getClassName().toString().equals("android.widget.Button");
+    }
+
+    // -------------------------------------------------------------------------- //
 
     private void openAppByNotification(AccessibilityEvent event) {
         if (event.getParcelableData() != null && event.getParcelableData() instanceof Notification) {
@@ -84,24 +158,57 @@ public class AutoLuckyMoneyService extends AccessibilityService {
         }
     }
 
-    private AccessibilityNodeInfo findClickable(@NonNull AccessibilityNodeInfo node) {
-        while (node != null) {
-            if (!node.isClickable())
-                node = node.getParent();
-            else
+    // -------------------------------------------------------------------------- //
+
+    private void getAllPackets(AccessibilityNodeInfo rootNode) {
+        if (rootNode == null) return;
+        switch (serviceMode) {
+            case 1:
+                // 不监听最新的红包，将屏幕上所有红包都过一遍
+                if (flagOnGet) {
+                    nodeList = rootNode.findAccessibilityNodeInfosByText("领取红包");
+
+//                    Toast.makeText(this, "We've got " + nodeList.size() + " packets", Toast.LENGTH_SHORT).show();
+                    MyHandler.index = nodeList.size() - 1;
+                    flagOnGet = false;      // 阻塞，不让表更新
+                }
+                handler.sendEmptyMessage(MyHandler.FLAG_CLICK_PACKETS);
+
+                break;
+            default:
+                nodeList = rootNode.findAccessibilityNodeInfosByText("领取红包");
+                if (!nodeList.isEmpty()) {
+                    Toast.makeText(this, "We've got " + nodeList.size() + " packets", Toast.LENGTH_SHORT).show();
+
+                    // 始终获取最后一个
+
+                    clickOpenPacket(nodeList.get(nodeList.size() - 1));
+                }
                 break;
         }
-        return node;
+
     }
 
-    private void goBackPage() {
-        AccessibilityNodeInfo nodeInfo = getRootInActiveWindow();
-        if (nodeInfo != null) {
-            performGlobalAction(AccessibilityService.GLOBAL_ACTION_BACK);
-            nodeInfo.recycle();
+    private void clickOpenPacket(AccessibilityNodeInfo node) {
+        AccessibilityNodeInfo clickableNode = findClickable(node);
+
+        if (clickableNode == null) return;
+        clickableNode.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+    }
+
+    private AccessibilityNodeInfo findClickable(AccessibilityNodeInfo node) {
+
+        AccessibilityNodeInfo nodeOut = node;
+        while (nodeOut != null) {
+            if (!nodeOut.isClickable()) {
+                nodeOut = nodeOut.getParent();
+            } else
+                break;
         }
-
+        return nodeOut;
     }
+
+    // -------------------------------------------------------------------------- //
 
     private void printEventLog(AccessibilityEvent event) {
         Log.i(TAG, "-------------------------------------------------------------");
@@ -151,5 +258,71 @@ public class AutoLuckyMoneyService extends AccessibilityService {
         }
 
         Log.i(TAG, "-------------------------------------------------------------");
+    }
+
+    /**
+     * 将节点对象的id和红包上的内容合并 Content@Id
+     * 用于表示一个唯一的红包
+     *
+     * @param node 任意对象
+     * @return 红包标识字符串
+     */
+    private String getHongbaoHash(AccessibilityNodeInfo node) {
+        /* 获取红包上的文本 */
+        String content;
+        try {
+            AccessibilityNodeInfo i = node.getParent().getChild(0);
+            content = i.getText().toString();
+        } catch (NullPointerException npr) {
+            return null;
+        }
+
+        return content + "@" + getNodeId(node);
+    }
+
+    /**
+     * 获取节点对象唯一的id，通过正则表达式匹配
+     * AccessibilityNodeInfo@后的十六进制数字
+     *
+     * @param node AccessibilityNodeInfo对象
+     * @return id字符串
+     */
+    private String getNodeId(AccessibilityNodeInfo node) {
+        /* 用正则表达式匹配节点Object */
+        Pattern objHashPattern = Pattern.compile("(?<=@)[0-9|a-z]+(?=;)");
+        Matcher objHashMatcher = objHashPattern.matcher(node.toString());
+
+        // AccessibilityNodeInfo必然有且只有一次匹配，因此不再作判断
+        objHashMatcher.find();
+
+        return objHashMatcher.group(0);
+    }
+
+    private static class MyHandler extends Handler {
+        private WeakReference<AutoLuckyMoneyService> reference;
+        private static final int FLAG_CLICK_PACKETS = 0x1001;
+
+        private static int index;
+
+        private MyHandler(AutoLuckyMoneyService service) {
+            reference = new WeakReference<>(service);
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            if (index >= 0) {
+                String id = reference.get().getHongbaoHash(reference.get().nodeList.get(index));
+                if (id != null && reference.get().nodeHashName.add(id)) {
+                    reference.get().clickOpenPacket(reference.get().nodeList.get(index));
+
+                    Log.i(reference.get().TAG, "此节点的时间+标志位\t\t" + System.currentTimeMillis() + "\t" + id);
+                }
+                Log.i(reference.get().TAG, "Packet index is " + index);
+                index--;
+            }
+            else if (index == -1)
+                reference.get().flagOnGet = true;
+        }
     }
 }
--- WeChat_Hongbao/app/src/main/java/cn/edu/uestc/wechat_hongbao/MainActivity.java
+++ WeChat_Hongbao/app/src/main/java/cn/edu/uestc/wechat_hongbao/MainActivity.java
@@ -7,19 +7,28 @@ import android.media.audiofx.BassBoost;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button btnToSettingsPage;
+    private Button btnModifyMode;
+
+    String currentText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        currentText = "普通模式";
+
         btnToSettingsPage = (Button) findViewById(R.id.start_service_page);
+        btnToSettingsPage.setOnClickListener(this);
+        btnModifyMode = (Button) findViewById(R.id.mode_change);
+        btnModifyMode.setOnClickListener(this);
     }
 
     @Override
@@ -27,7 +36,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onPostResume();
 
         btnToSettingsPage.setText("前往设置界面");
-        btnToSettingsPage.setOnClickListener(this);
+        btnModifyMode.setText(currentText);
     }
 
     @Override
@@ -36,6 +45,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.start_service_page:
                 Intent intent = new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
                 startActivity(intent);
+                break;
+            case R.id.mode_change:
+                switch (btnModifyMode.getText().toString()) {
+                    case "全取模式":
+                        currentText = "普通模式";
+                        btnModifyMode.setText("普通模式");
+                        AutoLuckyMoneyService.serviceMode = 0;
+                        Log.i(this.getClass().getSimpleName(), AutoLuckyMoneyService.serviceMode+"");
+                        break;
+                    default:
+                        currentText = "全取模式";
+                        btnModifyMode.setText("全取模式");
+                        AutoLuckyMoneyService.serviceMode = 1;
+                        Log.i(this.getClass().getSimpleName(), AutoLuckyMoneyService.serviceMode+"");
+                        break;
+                }
                 break;
             default:
                 break;
